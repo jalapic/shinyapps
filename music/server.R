f4963b620eb2ad7966775f7a08fe3c5ab8d729e4
James Curley <jc3181@columbia.edu>
2015-02-06 22:06:49 -0500

    add music social graphs
    add music social graphs

---
+++ music/server.R
@@ -0,0 +1,140 @@
+library(shiny)
+library(ggplot2)
+library(grid)
+library(dplyr)
+
+
+
+#import data
+newdf <- read.csv("newdf.csv", stringsAsFactors=F)
+
+
+
+# set colors
+mycolors <- c("#E41A1C", "#377EB8", "#4DAF4A",
+              "#984EA3", "#FF7F00", "#FFFF33",
+              "#A65628", "#F781BF", "#999999",
+              "#FFE4C4", "#E0EEEE")
+
+
+
+
+
+shinyServer(function(input, output) {
+  
+
+
+#### Reading in data and setting up dataframe
+
+
+
+  
+  df1 <- reactive({
+    
+    mintime <- input$timeper[1]
+    maxtime <- input$timeper[2]
+    
+    df1 <- 
+      newdf %>% filter(Time >= mintime & Time <= maxtime)
+    
+    ranks <- input$checkGroup
+    
+    if (input$winloser == 1) {
+      
+    df1 <- df1[((df1$winner %in% ranks) == T),]
+    }
+    
+    
+    else if (input$winloser == 2) {
+      
+    df1 <- df1[((df1$winner %in% ranks) == T | (df1$loser %in% ranks) == T),]
+    }
+    
+    
+    else
+      
+    df1 <- df1[((df1$loser %in% ranks) == T),]
+    
+})
+  
+
+
+  output$MyPlot <- renderPlot({
+    
+  # doing if/else to reactively change line-width / horizontal grid line colors ... must be a better way...
+    
+    if (input$linewt==1 & input$horizline==1) {
+        
+      ggplot() +
+      geom_segment(aes(x = Time, xend = Time, y = winner, yend = loser, color=factor(winner)),
+                   arrow=arrow(length = unit(.3, "cm")), lwd=.75, 
+      data=df1())  +
+      geom_point(aes(x = Time, y = winner, color = factor(winner)), size=3, data=df1()) + 
+      coord_cartesian(ylim=c(0.5,11.5)) +
+      scale_y_reverse(breaks=1:11) +
+      scale_color_manual(values = mycolors, limits=c(1:11)) +
+      theme_bw() +
+      theme(legend.position = "none",
+            panel.grid.major.y = element_line(color = mycolors, size=1),
+            axis.text.y = element_text(color = mycolors)) +
+      ylab("Animal rank")
+    }
+
+    else
+      
+    if (input$linewt==1 & input$horizline==2) {
+      
+      ggplot() +
+        geom_segment(aes(x = Time, xend = Time, y = winner, yend = loser, color=factor(winner)),
+                     arrow=arrow(length = unit(.3, "cm")), lwd=.75, 
+                     data=df1())  +
+        geom_point(aes(x = Time, y = winner, color = factor(winner)), size=3, data=df1()) + 
+        coord_cartesian(ylim=c(0.5,11.5)) +
+        scale_y_reverse(breaks=1:11) +
+        scale_color_manual(values = mycolors, limits=c(1:11)) +
+        theme_bw() +
+        theme(legend.position = "none",
+              panel.grid.major.y = element_line(size=1)) +
+        ylab("Animal rank")
+    }
+    
+    else
+
+    if (input$linewt==2 & input$horizline==1) {
+        
+    ggplot() +
+      geom_segment(aes(x = Time, xend = Time, y = winner, yend = loser, color=factor(winner)),
+                   arrow=arrow(length = unit(.3, "cm")), lwd=.7, 
+                   data=df1())  +
+      geom_point(aes(x = Time, y = winner, color = factor(winner)), size=3, data=df1()) + 
+      coord_cartesian(ylim=c(0.5,11.5)) +
+      scale_y_reverse(breaks=1:11)+
+      scale_color_manual(values = mycolors, limits=c(1:11)) +
+      theme_bw() +
+      theme(legend.position = "none",
+       panel.grid.major.y = element_line(color = mycolors, size=1),
+       axis.text.y = element_text(color = mycolors)) +
+  ylab("Animal rank")
+    
+}
+
+else
+
+  ggplot() +
+  geom_segment(aes(x = Time, xend = Time, y = winner, yend = loser, color=factor(winner)),
+               arrow=arrow(length = unit(.3, "cm")), lwd=.7, 
+               data=df1())  +
+  geom_point(aes(x = Time, y = winner, color = factor(winner)), size=3, data=df1()) + 
+  coord_cartesian(ylim=c(0.5,11.5)) +
+  scale_y_reverse(breaks=1:11)+
+  scale_color_manual(values = mycolors, limits=c(1:11)) +
+  theme_bw() +
+  theme(legend.position = "none",
+        panel.grid.major.y = element_line(size=1)) +
+  ylab("Animal rank")
+
+    
+    })
+  
+  
+})
---
+++ music/ui.R
@@ -0,0 +1,83 @@
+library(shiny)
+library(ggplot2)
+library(grid)
+library(dplyr)
+
+shinyUI(fluidPage(
+  headerPanel("Music Notation Plots of Social Interactions"),
+  
+    plotOutput("MyPlot"),
+  
+hr(),
+br(),
+br(),
+
+
+fluidRow(
+    column(5,
+           
+           checkboxGroupInput("checkGroup", label = h3("Ranks to Include"), 
+                              choices = list("1" = 1, "2" = 2, "3" = 3, 
+                                             "4" = 4, "5" = 5, "6" = 6, 
+                                             "7" = 7, "8" = 8, "9" = 9, 
+                                             "10" = 10, "11" = 11),
+                              selected = c(3),
+                              inline = T),
+           
+           radioButtons("winloser", "Who to include",
+                        choices = list("Winners only" = 1, 
+                                       "Winners & Losers" = 2,
+                                       "Losers only" = 3), 
+                        selected = 2,
+                        inline=T)
+           
+           
+    ),
+    
+   
+    column(4,
+           
+    sliderInput("timeper", label = h3("Time Period"), 1, 999, value = c(1,999), step=10, ticks=T)
+    
+    ),
+    
+
+    column(3,
+           radioButtons("linewt", label = h3("Line & Arrow Weight"),
+                 choices = list("Heavy" = 1, "Light" = 2), 
+                 selected = 1,
+                 inline=T),
+           
+           radioButtons("horizline", label = h3("Grid Line Color"),
+                        choices = list("Colors" = 1, "Gray" = 2), 
+                        selected = 2,
+                        inline=T)
+           
+           
+           
+    )
+    ),
+
+br(),
+
+wellPanel(
+  
+  helpText("This music notation plot for visualizing the temporal sequence and directionality of social interaction data is based upon ", 
+           a(href ="http://www.frontiersinzoology.com/content/3/1/18", "this 2006 paper by Ivan Chase",target = "_blank")),
+  
+  helpText(h5("Author: James Curley")),
+  
+  helpText(p("Please contact",
+             a(href ="https://twitter.com/jalapic", "James on twitter",target = "_blank"),
+             " or at his",
+             a(href ="http://curleylab.psych.columbia.edu/curley.html", "research page", target = "_blank"),
+             ", for more information, to suggest improvements or report errors.")),
+
+  helpText(p("All code are available at ",
+             a(href ="https://github.com/jalapic/shinyapps", "my GitHub page",target = "_blank")
+  ))
+) 
+
+
+
+  ))
